a93f084b762bb84d327b75a4997b39f0c405b1fa
beomhyunkim <beomhyunkim@gmail.com>
2019-10-23 14:09:31 +0900

    getAVGprice

---
+++ src/main/java/com/project/k51star/controller/ApiController.java
@@ -0,0 +1,12 @@
+package com.project.k51star.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApiController {
+	
+	@RequestMapping(value = "/api")
+	public void api() {}
+	
+}
